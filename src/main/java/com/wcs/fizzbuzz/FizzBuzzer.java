c701315038ff3749baa14f1f09f45fff10eedc70
Jugis <vargapalmark92@gmail.com>
2015-03-04 14:56:04 +0100

    asd

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,7 +3,28 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       if (number / 7 == 0) return "Wizz"; else
+        if (tartalmazSzamot(number, '3') == true) return "Fizz";else
+        if (tartalmazSzamot(number, '5') == true) return "Buzz";else
+        if (tartalmazSzamot(number, '7') == true) return "Wizz";else
+        return String.valueOf(number);
+        }
+    
+     
+    
+    
+
+    private boolean tartalmazSzamot(Integer number, char c) {
+        //igen utólag rájöttem, hogy ez teljesen kihagyható lett volna, de már mindegy
+        boolean vaneSzam = false;
+        String intToString = String.valueOf(number);
+        
+        for (int i = 0; i < intToString.length(); i++) {
+            if (intToString.charAt(i) == c) {
+                vaneSzam = true;
+            }
+        }
+        return vaneSzam;
     }
     
 }
